62d59f4c9ca236f29f58f0998107545ebcc36a23
pavitra301234 <pavitrak3028@gmail.com>
2025-10-30 19:07:12 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/assetlinks.json",
+        destination: "/well-known/assetlinks.json",
+      },
+    ]
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
